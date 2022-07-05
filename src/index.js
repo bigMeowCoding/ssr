a2c3f40947e6496ac254598889937f9e96b37c30
zhouyijun <zhou495248579@hotmail.com>
2022-07-05 23:41:51 +0800

    feat:简单的服务端渲染demo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const { renderToString } = require("react-dom/server");
+import Home from "./containers/home";
+import React from "react";
 const app = express();
 
+const content = renderToString(<Home />);
 app.get("/", (req, res) => {
   res.send(`<html>
 			<head>
 				<title>hello</title>
 			</head>
 			<body>
-				<h1>first lesson</h1>
-				<p>hello world</p>
+			${content}
 			</body>
   	</html>`);
 });
