bac5ddf9a04f6266575509999a8392924963f682
zhouyijun <zhou495248579@hotmail.com>
2022-07-06 21:33:32 +0800

    chore:优化webpack 配置

--- webpack.client.js
+++ webpack.client.js
@@ -1,21 +1,12 @@
 const path = require("path");
-const nodeExternals = require("webpack-node-externals");
-
-module.exports = {
+const config = require("./webpack.base");
+const { merge } = require("webpack-merge");
+const clientConfig = {
   target: "web",
-  mode: "development",
   entry: "./src/client/index.js",
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "public"),
   },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: "babel-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
 };
+module.exports = merge(config, clientConfig);
