8a94ac103013d91263cc97c42eb90c5fcf64960f
zhouyijun <zhou495248579@hotmail.com>
2022-07-07 23:03:27 +0800

    chore:add proxy

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import request from "umi-request";
 
 // 同构：一套React代码，在服务器端执行一次，再客户端再执行一次
 
 const Home = () => {
+  useEffect(() => {
+    request
+      .post("/api3/test", {
+        data: {},
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   return (
     <div>
       <div>This is Dell Lee!</div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ const express = require("express");
 const { renderToString } = require("react-dom/server");
 import Home from "./containers/home";
 import React from "react";
+import proxy from "express-http-proxy";
 const app = express();
 app.use(express.static("public"));
 const content = renderToString(<Home />);
+app.use(
+  "/api3",
+  proxy("http://localhost:8888", {
+    proxyReqPathResolver: function (req) {
+        console.log('req====url',req.url)
+      return req.url
+    },
+  })
+);
 app.get("/", (req, res) => {
   res.send(`<html>
 			<head>
